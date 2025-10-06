66a5e54542529d93dce98040dca0dcfc55df305a
SSM-UP <mika.music@uni-potsdam.de>
2025-10-06 16:45:02 +0200

    Create shutter_on-off.ino
    added lagecy shutter control code

---
+++ shutter_on-off.ino
@@ -0,0 +1,15 @@
+int PowerPin = 10;               
+
+void setup()
+{
+  pinMode(PowerPin, OUTPUT);     
+  digitalWrite(PowerPin, HIGH);  
+  delay(1800000);
+  digitalWrite(PowerPin, LOW); 
+}
+
+void loop()
+{
+
+
+}
